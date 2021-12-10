2fceae79ddb0143c2ef91046bc027cd1aefa47a8
Aykut Kardaş <aykutkrds@gmail.com>
2021-12-11 02:57:48 +0300

    Add `react-icomoon` module

---
+++ src/components/Icon/index.jsx
@@ -0,0 +1,8 @@
+import IcoMoon from "react-icomoon";
+const iconSet = require("./selection.json");
+
+const Icon = ({ ...props }) => {
+  return <IcoMoon iconSet={iconSet} {...props} />;
+};
+
+export default Icon;
