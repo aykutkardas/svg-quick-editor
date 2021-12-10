6e99972fc34997a328e5aaf43787f96dda964272
Aykut Kardaş <aykutkrds@gmail.com>
2021-12-11 00:19:10 +0300

    Add `IconList` component

---
+++ src/components/IconList/index.jsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import cx from "classnames";
+
+import * as styles from "./IconList.module.css";
+
+import { Context } from "../../contexts/FilesContext";
+
+const IconList = () => {
+  const { files, selectedFile, setSelectedFile } = useContext(Context);
+
+  return (
+    <div className={styles.IconList}>
+      <h4 className={styles.IconListMainTitle}>EXPLORER</h4>
+      <h4 className={styles.IconListTitle}>File</h4>
+      {files.map((file) => (
+        <div
+          key={file.name}
+          className={cx(styles.IconListItem, {
+            [styles.IconListItemActive]: file.name === selectedFile?.name,
+          })}
+          onClick={() => setSelectedFile(file)}
+        >
+          <span
+            className={styles.IconListItemIcon}
+            dangerouslySetInnerHTML={{ __html: file.content }}
+          />
+          {file.name}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default IconList;
