ed166b5d8992f5ce7c518054201369d125f79a86
Aykut Kardaş <aykutkrds@gmail.com>
2021-12-11 13:14:46 +0300

    Add `DownloadButton` component

---
+++ src/components/DownloadButton/index.jsx
@@ -0,0 +1,29 @@
+import cx from "classnames";
+import Icon from "../../components/Icon";
+import convertToSVG from "../../utils/convertToSVG";
+
+import * as styles from "./DownloadButton.module.css";
+
+const Download = ({ file }) => {
+  const onClick = () => {
+    const content = convertToSVG(file);
+
+    var dataStr =
+      "data:image/svg+xml;charset=utf-8," + encodeURIComponent(content);
+    var downloadElement = document.createElement("a");
+    downloadElement.setAttribute("href", dataStr);
+    downloadElement.setAttribute("download", file.name);
+    downloadElement.click();
+  };
+
+  return file ? (
+    <button
+      className={cx("download-button", styles.DownloadButton)}
+      onClick={onClick}
+    >
+      <Icon icon="download" size={14} />
+    </button>
+  ) : null;
+};
+
+export default Download;
