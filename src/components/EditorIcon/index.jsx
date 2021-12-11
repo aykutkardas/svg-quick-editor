b7438a3923ee2df20702a6be6ba0597baad811a7
Aykut Kardaş <aykutkrds@gmail.com>
2021-12-11 19:14:51 +0300

    Split `EditorIcon` component

---
+++ src/components/EditorIcon/index.jsx
@@ -0,0 +1,51 @@
+import { useContext, useEffect, useState } from "react";
+import cx from "classnames";
+
+import EditorIconInfo from "../EditorIconInfo";
+
+import { Context } from "../../contexts/FilesContext";
+
+import * as styles from "./EditorIcon.module.css";
+
+const EditorIcon = () => {
+  const { files, selectedFile, getSelectedFile, activePathIndex } =
+    useContext(Context);
+  const [file, setFile] = useState(getSelectedFile(selectedFile));
+
+  useEffect(() => {
+    setFile(getSelectedFile(selectedFile));
+  }, [selectedFile, files]);
+
+  const isCurrentPath = (activePathIndex, index) => {
+    return typeof activePathIndex === "number"
+      ? activePathIndex === index
+      : true;
+  };
+
+  return (
+    <div className={styles.EditorIcon}>
+      {file && (
+        <div className={styles.EditorIconFrame}>
+          <svg className={styles.EditorIconSVG} viewBox={file.viewBox}>
+            {file.paths.map((path, index) => (
+              <path
+                key={path + index + (file.fills[index] || "")}
+                d={path}
+                fill={file.fills[index] || "#999"}
+                className={cx(styles.EditorCurrentIconPath, {
+                  [styles.EditorCurrentIconPathPassive]: !isCurrentPath(
+                    activePathIndex,
+                    index
+                  ),
+                })}
+              />
+            ))}
+          </svg>
+          <EditorIconInfo file={file} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default EditorIcon;
--- src/containers/Editor/index.jsx
+++ src/containers/Editor/index.jsx
@@ -1,56 +1,14 @@
-import { useContext, useEffect, useState } from "react";
-import cx from "classnames";
-
 import * as styles from "./Editor.module.css";
-
 import IconList from "../../components/IconList";
 import EditorTool from "../../components/EditorTool";
-import Icon from "../../components/Icon";
-import EditorIconInfo from "../../components/EditorIconInfo";
-
-import { Context } from "../../contexts/FilesContext";
+import EditorIcon from "../../components/EditorIcon";
 
 const Editor = () => {
-  const { files, selectedFile, getSelectedFile, activePathIndex } =
-    useContext(Context);
-  const [file, setFile] = useState(getSelectedFile(selectedFile));
-
-  useEffect(() => {
-    setFile(getSelectedFile(selectedFile));
-  }, [selectedFile, files]);
-
-  const isCurrentPath = (activePathIndex, index) => {
-    return typeof activePathIndex === "number"
-      ? activePathIndex === index
-      : true;
-  };
-
   return (
     <div className={styles.Editor}>
       <IconList />
       <div className={styles.EditorIconArea}>
-        <div className={styles.EditorIcon}>
-          {file && (
-            <div className={styles.EditorIconFrame}>
-              <svg className={styles.EditorIconSVG} viewBox={file.viewBox}>
-                {file.paths.map((path, index) => (
-                  <path
-                    key={path + index + (file.fills[index] || "")}
-                    d={path}
-                    fill={file.fills[index] || "#999"}
-                    className={cx(styles.EditorCurrentIconPath, {
-                      [styles.EditorCurrentIconPathPassive]: !isCurrentPath(
-                        activePathIndex,
-                        index
-                      ),
-                    })}
-                  />
-                ))}
-              </svg>
-              <EditorIconInfo file={file} />
-            </div>
-          )}
-        </div>
+        <EditorIcon />
         <EditorTool />
       </div>
     </div>
