3bde8db60484bfd35b44b75187e109e2edb91b0a
Aykut Kardaş <aykutkrds@gmail.com>
2021-12-11 11:54:53 +0300

    Remove useless line on `App.jsx`

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import "./App.css";
 
-import { useContext } from "react";
-
-import Upload from "./components/Upload";
 import Editor from "./containers/Editor";
-import { Context } from "./contexts/FilesContext";
 
 function App() {
-  // const { files } = useContext(Context);
-
   return (
     <div className="App">
       <Editor />
